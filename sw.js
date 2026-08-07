48984d9920d93a6261638a9ac66d99f27f8a7499
Productions iMédias <philippe.beaubien@gmail.com>
2026-08-07 16:16:23 -0400

    Freemium: 3 modules gratuits (essai 7j), reste verrouille sans licence + bump SW

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Au premier chargement, il met en cache tous les fichiers de l'app.
    Ensuite, l'app se lance sans réseau, exactement comme une app native. */
 
-const CACHE = "mecaquest-v2";
+const CACHE = "mecaquest-v3";
 const ASSETS = [
   "index.html",
   "app.js",
